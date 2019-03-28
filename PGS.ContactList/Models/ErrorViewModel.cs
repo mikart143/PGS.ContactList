f8e61616587de34bbc4ca64f10f8083e449a1ba6
Mikart <mikart143@gmail.com>
2019-03-28 11:55:08 +0100

    Add project files.

---
+++ PGS.ContactList/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PGS.ContactList.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
